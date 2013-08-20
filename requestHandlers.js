f1e93b0b9106de7692b7d4e3b8505130a8a8eaa6
Todd Guerra <tguerra@crownpartners.com>
2013-08-20 15:05:58 -0400

    using a variable for the insult

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,15 @@
 
 function insult(response) {
+	
+	var finishedInsult = "You're a smelly butthole";
+
 	var body = '<html>' +
 	'<head>'+
     '<meta http-equiv="Content-Type" '+
     'content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<p>You are about to be insulted, pal. <p>' +
+    '<p>' + finishedInsult + '<p>' +
     '</body>' +
     '</html>';
 
