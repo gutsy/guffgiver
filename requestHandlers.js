703a40786f0d8ed8fb8811f48f31ab6156f15e8e
Todd Guerra <tguerra@crownpartners.com>
2013-08-20 16:10:38 -0400

    finished this stupid exercise. I'll style it better....someday

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,20 @@
 
 function insult(response) {
 	
-	var finishedInsult = "You're a smelly butthole";
+	var part1 = ["Your face", "Your butt", "Your stupid fingers", "Your mamma", "Your brain", "Everything about you", "Even your stupid right foot", "Your ugly earlobe", "Your breath", "Your left knee"];
+
+	var part2 = ["looks like", "smells like", "resembles", "feels like", "is uglier than", "is more stupid than", "is worse than", "looks scarier than", "is akin to", "is about equal to"];
+
+	var part3 = ["a warm", "a dead", "a bubbly", "a rotten", "a smelly", "a wet", "an angry", "an exploding", "a dying", "a bleeding, dying, smelly, horrible"];
+
+	var part4 = ["rat", "monkey", "butthole", "cadaver", "antelope carcass", "Kardashian sister", "Yeti rectum", "pile of oranges", "herd of goats", "apple"];
+
+	var index1 = Math.floor((Math.random()*10)+1);
+	var index2 = Math.floor((Math.random()*10)+1);
+	var index3 = Math.floor((Math.random()*10)+1);
+	var index4 = Math.floor((Math.random()*10)+1);
+
+	var finishedInsult = part1[index1 - 1] + " " + part2[index2 -1] + " " + part3[index3 - 1] + " " + part4[index4 - 1] + "!";
 
 	var body = '<html>' +
 	'<head>'+
